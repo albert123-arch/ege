f63d34f04b10166c0334107479af013b2fae2272
albert123-arch <iskhakovalbert1@gmail.com>
2026-05-07 21:15:02 +0500

    Update db.php
    error handle

--- includes/db.php
+++ includes/db.php
@@ -5,7 +5,7 @@ $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
 if ($mysqli->connect_errno) {
 	http_response_code(500);
-	die('Database connection error.');
+	die('Database connection error: ' . $mysqli->connect_error);
 }
 
 $mysqli->set_charset('utf8mb4');

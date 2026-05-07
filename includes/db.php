7f02f7835959df7744088c395878e9d8915422eb
albert123-arch <iskhakovalbert1@gmail.com>
2026-05-07 21:27:32 +0500

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,11 +1,17 @@
 <?php
 require_once __DIR__ . '/config.php';
 
-$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-
-if ($mysqli->connect_errno) {
+if (!class_exists('mysqli')) {
 	http_response_code(500);
-	die('Database connection error: ' . $mysqli->connect_error);
+	die('Database connection error: mysqli extension is not enabled.');
 }
 
-$mysqli->set_charset('utf8mb4');
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+try {
+	$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+	$mysqli->set_charset('utf8mb4');
+} catch (Throwable $exception) {
+	http_response_code(500);
+	die('Database connection error: ' . $exception->getMessage());
+}
